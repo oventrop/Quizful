7aad0753420d251f10df4a20ae71d88dff2d3460
oventrop <oventrop@tut.by>
2017-09-27 18:13:01 +0300

    comments added

--- src/main/java/QuizfulTest/Main.java
+++ src/main/java/QuizfulTest/Main.java
@@ -8,25 +8,25 @@ public class Main {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+		// opening browser, loading mainpage
 		MainPage main = new MainPage(driver);
 		main.openBrowser();
 		main.goToLoginPage();
-		
+
+		// login to account
 		LoginPage login = new LoginPage(driver);
 		login.loginToAccount();
-		
+
+		//move to user profile page 
 		UserMainPage userMain = new UserMainPage(driver);
 		userMain.goToUserAccount();
-		
+
+		//editing user profile
 		UserProfile userProfile = new UserProfile(driver);
 		userProfile.goToUserPreferences();
-		
 		EditProfile edit = new EditProfile(driver);
 		edit.editMainPrefs();
-			
 		edit.editConfidential();
-		
 		edit.editNotifications();
 		edit.addAvatar();
 		edit.saveInfo();
